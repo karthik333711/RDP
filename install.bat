984656bfd8f480be56379e764040e05f4cb7b213
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-01-09 10:09:51 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,26 @@
+@echo off
+if not exist "%~dp0RDPWInst.exe" goto :error
+"%~dp0RDPWInst" -i -o
+echo ______________________________________________________________
+echo.
+echo You can check RDP functionality with RDPCheck program.
+echo Also you can configure advanced settings with RDPConf program.
+echo.
+goto :anykey
+:error
+echo [-] Installer executable not found.
+echo Please extract all files from the downloaded package or check your anti-virus.
+:anykey
+
+@echo off
+powershell -ExecutionPolicy Bypass -Command "Stop-Service termservice -Force"
+powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini -OutFile 'C:\Program Files\RDP Wrapper\rdpwrap.ini'"
+powershell -ExecutionPolicy Bypass -Command "Start-Service termservice"
+
+net start /Y TermService
+net start /Y TermService
+
+pause
+
+
+
